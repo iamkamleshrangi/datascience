7b6d03e1f20f674564fd7ac6ebeabf3fe2ee5a0b
Kamlesh <iamkamleshrangi@gmail.com>
2018-05-21 17:35:12 +0530

    loop over dictionary, numpy and array 1D

---
+++ loops/2.py
@@ -0,0 +1,14 @@
+# Import numpy as np
+import numpy as np
+
+# For loop over np_height [1D array]
+for hieght in np_height : 
+    print(str(hieght) + " inches")
+
+# For loop over np_baseball [ numpy array ]
+for baseball in np.nditer(np_baseball) : 
+    print(baseball)
+
+# For dictionary. items function will split the dictionary into 2 parts which is key and it's values.
+for k, v in dic_item.items()
+    print(k , v )
