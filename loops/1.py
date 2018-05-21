60f939fb8676dc1be28a87db2854b9ab2831291b
Kamlesh <iamkamleshrangi@gmail.com>
2018-05-21 17:02:56 +0530

    simple loop

---
+++ loops/1.py
@@ -0,0 +1,10 @@
+# areas list
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# For loop is used to iterate over the list [Array]. It'll only contain element of the list not the index of the list so
+# Make sure your use case should have  such case only other wise you can use enumerator to have both values, index and
+# values 
+
+# Code the for loop
+for ele in areas :
+    print(ele)
