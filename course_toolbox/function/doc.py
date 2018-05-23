f6e8aacbfd52b5690069a213ed395a540bed2473
Kamlesh <iamkamleshrangi@gmail.com>
2018-05-23 10:35:08 +0530

    Doc about the function

---
+++ course_toolbox/function/doc.py
@@ -0,0 +1,9 @@
+""" This is called as function help to reuseability of the code """
+
+""" def is a keyword which is used to define the function and the point from where the function starts """
+def run(value) : """ <== This is called as function header, having one parameter which is as value """
+    return value ** 2  """ <= indicate operation on the function which is, it'll do the square of the value and return
+    to the respective one """
+
+print(run(5)) """ <== We call is as function call with the argument 5 and based on calculation over 5 for the square,
+it'll do it and send back to respective ones, for here is like print statement  """
