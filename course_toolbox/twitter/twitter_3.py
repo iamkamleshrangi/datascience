7792a8d208da9c168d97ac0a0c74554ee24df058
Kamlesh <iamkamleshrangi@gmail.com>
2018-06-02 12:46:14 +0530

    Third file

---
+++ course_toolbox/twitter/twitter_3.py
@@ -0,0 +1,10 @@
+# Select retweets from the Twitter DataFrame: result
+result = filter(lambda tweets: tweets[0:2] == 'RT', tweets_df['text'])
+
+# Create list from filter object result: res_list
+res_list = list(result)
+
+# Print all retweets in res_list
+for tweet in res_list:
+    print(tweet)
+
