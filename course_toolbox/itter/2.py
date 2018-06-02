dcfb412b63a774934dab8d3519767d5edc720267
Kamlesh <iamkamleshrangi@gmail.com>
2018-06-02 15:34:09 +0530

    Add file

---
+++ course_toolbox/itter/2.py
@@ -0,0 +1,21 @@
+# Create a list of strings: mutants
+mutants = ['charles xavier', 
+            'bobby drake', 
+            'kurt wagner', 
+            'max eisenhardt', 
+            'kitty pride']
+
+# Create a list of tuples: mutant_list
+mutant_list = (mutants)
+
+# Print the list of tuples
+print(mutant_list)
+
+# Unpack and print the tuple pairs
+for index1, value1 in enumerate(mutant_list):
+    print(index1, value1)
+
+# Change the start index
+for index2, value2 in enumerate(mutant_list, start= 1):
+    print(index2, value2)
+
