d1c5816748452a5885c47ceb39b4b604d3256bce
Kamlesh <iamkamleshrangi@gmail.com>
2018-06-03 23:55:44 +0530

    list comprehension first program

---
+++ course_toolbox2/list_comprehension.py
@@ -0,0 +1,8 @@
+'''You now have all the knowledge necessary to begin writing list comprehensions! Your job in this exercise is to write
+a list comprehension that produces a list of the squares of the numbers ranging from 0 to 9.'''
+
+# list comprehensions 
+# Create list comprehension: squares
+squares = [ i *i  for i in range(0,10)]
+print(squares)
+
