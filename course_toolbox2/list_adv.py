bece329c69648a9c0611825fd2ead6ca2594a340
Kamlesh <iamkamleshrangi@gmail.com>
2018-06-07 11:38:26 +0530

    Add list to the repo

---
+++ course_toolbox2/list_adv.py
@@ -0,0 +1,12 @@
+# Zip lists: zipped_lists
+zipped_lists = zip(feature_names, row_vals)
+
+# Create a dictionary: rs_dict
+rs_dict = dict(zipped_lists)
+
+# Create a list: rs_list 
+rs_list = dict(zipped_lists)
+
+# Print the dictionary
+print(rs_dict)
+
