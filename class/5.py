dec9ee5895a653a5ba2cf1f397fb692082dbd514
Kamlesh <iamkamleshrangi@gmail.com>
2018-06-10 08:22:06 +0530

    sample file

---
+++ class/5.py
@@ -0,0 +1,13 @@
+# Import numpy
+import numpy as np
+
+# Assign the filename: file
+file = 'digits_header.txt'
+
+# Load the data: data
+data = np.loadtxt(file, delimiter='\t', skiprows=1, usecols=[1,3])
+
+# Print data
+print(data)
+
+
