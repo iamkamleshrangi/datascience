d912bf29fcf7236f3873f9f6556c094121c4ee32
Kamlesh <iamkamleshrangi@gmail.com>
2018-06-10 18:57:25 +0530

    excel sheets

---
+++ samples/15.py
@@ -0,0 +1,12 @@
+# Import pandas
+import pandas as pd
+
+# Assign spreadsheet filename: file
+file = 'battledeath.xlsx'
+
+# Load spreadsheet: xl
+xl = pd.ExcelFile(file)
+
+# Print sheet names
+print(xl.sheet_names)
+
